a7aca063b96d7ea2588a987cdfc3cd47a104c384
Akhmed8251 <bratya_250@mail.ru>
2024-03-30 14:46:09 +0300

    исправил путь к api

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,4 @@
-// export const API_URL = "/api"
-export const API_URL = "https://localhost:7275"
+ export const API_URL = "/api"
 export const FILES_URL = "/Files"
 export const getAuthToken = () => {
     return localStorage.getItem("access_token")
