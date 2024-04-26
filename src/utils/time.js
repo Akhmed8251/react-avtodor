4dca572760ad43490e32b5a63e94749a731c2c0b
Akhmed8251 <bratya_250@mail.ru>
2024-04-26 21:34:31 +0300

    вернул формат времени

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,6 +1,4 @@
 export const formatTime = (date) => {
     const newDate = new Date(date)
-    const month = newDate.getMonth() + 1
-    const monthStr = month < 10 ? `0${month}` : month
-    return `${newDate.getDate()}.${monthStr}.${newDate.getFullYear()}`
+    return `${newDate.getFullYear()}-${newDate.getMonth() + 1}-${newDate.getDate()}`
 }
